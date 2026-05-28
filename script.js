904b299ad75a95a92e1018f95b4e5ee38609fb6d
Edson Rodrigues <edson.rodrigues9@fatec.sp.gov.br>
2026-05-28 00:19:18 -0300

    closes #4 - Implementar as quatro operações aritiméticas.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function somar() {
+function calc(operacao) {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
-    var resultado = n1 + n2;
+    switch(operacao) {
+    case "+":    var resultado = n1 + n2;   break;
+    case "-":    var resultado = n1 - n2;   break;
+    case "*":    var resultado = n1 * n2;   break;
+    case "/":    var resultado = n1 / n2;   break;
+    }
     document.getElementById("saida").value = resultado;
 }
-   
